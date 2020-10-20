c560d37c685fd6a6deb9abcf967fad1ea7cd3880
Yash Khatri <yashkhatri@Yashs-MacBook-Pro.local>
2020-10-20 21:08:45 +0100

    Initial setup of Spring boot with dependencies - spring web, JPA, flyway migration, H2

---
+++ src/test/java/com/personalprojects/authenticationrestservice/AuthenticationRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalprojects.authenticationrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
